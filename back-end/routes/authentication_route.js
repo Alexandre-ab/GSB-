fd8d11448c97b6b1f1995157a31923af1ecc57d8
Alexandre-ab <alexandre.boue72@gmail.com>
2025-06-12 22:34:07 +0200

    feat: Ajout du dossier front-end (React)

--- back-end/routes/authentication_route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { login } = require('../controllers/authentication_controller')
-
-router.post('/login', login)
-
-module.exports = router
