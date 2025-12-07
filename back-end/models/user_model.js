5b95f24fb021daa02fd54781c626d8b00b04978c
Alexandre <alexandre.boue72@gmail.com>
2025-12-08 00:18:40 +0100

    Fix: Corriger ordre définition User - déplacer hook pre('save') avant mongoose.model()

--- back-end/models/user_model.js
+++ back-end/models/user_model.js
@@ -1,24 +1,7 @@
 const mongoose = require('mongoose')
 const sha256 = require('js-sha256')
 
-const findOrCreateUser = async (profile) => {
-    const existingUser = await User.findOne({ email: profile.emails[0].value });
-  
-    if (existingUser) return existingUser;
-  
-    const newUser = new User({
-      name: profile.name.givenName + ' ' + profile.name.familyName,
-      email: profile.emails[0].value,
-      password: sha256(profile.id + process.env.SALT),
-      role: "user",
-      type_sso: "google",
-      external_id: profile.id
-    });
-  
-    await newUser.save();
-    return newUser;
-}
-
+// Définir le schéma d'abord
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -54,30 +37,52 @@ const userSchema = new mongoose.Schema({
     },
 })
 
+// Hook pre('save') - DOIT être défini AVANT mongoose.model()
 userSchema.pre('save', async function(next) {
-
     try {
         if(!process.env.SALT) {
             throw new Error('SALT is not defined', { cause: 501 })
         }
-    // Vérifier l'existence seulement pour les nouveaux utilisateurs
-    if (this.isNew) {
-        const existingUser = await User.findOne({ email: this.email })
-        if (existingUser && existingUser._id.toString() !== this._id.toString()) {
-            throw new Error('User already exists', { cause: 400 })
+        // Vérifier l'existence seulement pour les nouveaux utilisateurs
+        if (this.isNew) {
+            // Utiliser this.constructor au lieu de User pour éviter la référence circulaire
+            const UserModel = this.constructor
+            const existingUser = await UserModel.findOne({ email: this.email })
+            if (existingUser && existingUser._id.toString() !== this._id.toString()) {
+                throw new Error('User already exists', { cause: 400 })
+            }
         }
-    }
-    
-    // Hasher le mot de passe seulement s'il a été modifié ou si c'est un nouvel utilisateur
-    if (this.isModified('password') || this.isNew) {
-        this.password = sha256(this.password + process.env.SALT)
-    }
-    next()
+        
+        // Hasher le mot de passe seulement s'il a été modifié ou si c'est un nouvel utilisateur
+        if (this.isModified('password') || this.isNew) {
+            this.password = sha256(this.password + process.env.SALT)
+        }
+        next()
     } catch (error) {
         next(error)
     }
 })
 
+// Créer le modèle User APRÈS avoir défini les hooks
 const User = mongoose.model('User', userSchema)
+
+// Fonction findOrCreateUser - maintenant User est défini
+const findOrCreateUser = async (profile) => {
+    const existingUser = await User.findOne({ email: profile.emails[0].value });
+  
+    if (existingUser) return existingUser;
+  
+    const newUser = new User({
+      name: profile.name.givenName + ' ' + profile.name.familyName,
+      email: profile.emails[0].value,
+      password: sha256(profile.id + process.env.SALT),
+      role: "user",
+      type_sso: "google",
+      external_id: profile.id
+    });
+  
+    await newUser.save();
+    return newUser;
+}
 
 module.exports = { User, findOrCreateUser }
